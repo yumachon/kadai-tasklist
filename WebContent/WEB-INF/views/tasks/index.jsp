d9a43ca1e2a44ba84118cf6a4138b4a4e949a8db
yuma.takushima <takushima@valuedesign.jp>
2024-04-23 15:35:02 +0900

    ShowServlet.javaを作成

--- WebContent/WEB-INF/views/tasks/index.jsp
+++ WebContent/WEB-INF/views/tasks/index.jsp
@@ -9,7 +9,7 @@
                     <a href="${pageContext.request.contextPath}/show?id=${task.id}">
                         <c:out value="${task.id}" />
                     </a>
-                    ：<c:out value="${task.title}"></c:out> &gt; <c:out value="${task.content}" />
+                    ：<c:out value="${task.content}"></c:out>
                 </li>
             </c:forEach>
         </ul>
--- src/models/Task.java
+++ src/models/Task.java
@@ -26,9 +26,6 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
-
     @Column(name = "content", length = 255, nullable = false)
     private String content;
 
@@ -44,14 +41,6 @@ public class Task {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getContent() {
